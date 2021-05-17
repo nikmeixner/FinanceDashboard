75de53cf990868208f78bcae0c856a017e8307ec
nik <niklas-meixner@web.de>
2021-05-17 09:46:47 +0200

    proc

--- Tests/app_test.py
+++ Tests/app_test.py
@@ -6,7 +6,6 @@ from dash.dependencies import Input, Output
 import plotly.express as px
 import requests
 import yfinance as yf
-from yahoo_fin import stock_info as si
 import datetime as dt
 from pandas_datareader import data
 
